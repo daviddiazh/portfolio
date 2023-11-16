fcc087a2816115e35df412f00ffd93ea638b9443
daviddiazh <daviddiazlds1221@gmail.com>
2023-11-15 23:57:26 -0500

    feat: adding new menu on mobile and tablets devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import { useViewTransitions } from '@/hooks/useViewTransitions';
 import PresentationHome from '@/modules/PresentationHome';
 import styles from './page.module.css'
 
 export default function Home() {
 
-  const {navigate} = useViewTransitions();
-
   return (
     <main className={styles.main}>
+
       <PresentationHome />
 
-      {/* <p onClick={() => navigate('/projects')}>
-        Proyectos
-      </p> */}
+      
     </main>
   )
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,47 @@
-import React from 'react'
-import styles from './styles.module.css';
+import React, { useState } from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
+import styles from './styles.module.css';
+import openMenuIcon from '@/assets/icons/open-menu.svg';
+import closeMenuIcon from '@/assets/icons/close-menu.svg';
 
 export const Navbar = () => {
 
-  return (
-    <div className={styles.main_container}>
-      <Link href='/'><h1 className={styles.title}>David Diaz.</h1></Link>
+  const [ isOpenMenu, setOpenMenu ] = useState(false);
 
-      <div className={styles.links_container}>
-        {/* <a className={styles.link} onClick={() => navigate('/about')}>Sobre mi</a> */}
-        <Link className={styles.link} href='/#about'>Sobre mi</Link>
-        <Link className={styles.link} href='/#projects'>Proyectos</Link>
-        <Link className={styles.link} href='/#contact'>Contácto</Link>
+  const toggleMenu = () => setOpenMenu(!isOpenMenu);
+
+  return (
+    <>
+      <div className={styles.main_container}>
+        <Link href='/'><h1 className={styles.title}>David Diaz.</h1></Link>
+
+        <div className={styles.links_container}>
+          {/* <a className={styles.link} onClick={() => navigate('/about')}>Sobre mi</a> */}
+          <Link className={styles.link} href='/#about'>Sobre mi</Link>
+          <Link className={styles.link} href='/#projects'>Proyectos</Link>
+          <Link className={styles.link} href='/#contact'>Contácto</Link>
+        </div>
+
+        {
+          isOpenMenu
+          ? <Image src={closeMenuIcon} alt="Abrir menu, icono" className={styles.close_menu_icon} onClick={toggleMenu} />
+          : <Image src={openMenuIcon} alt="Abrir menu, icono" className={styles.open_menu_icon} onClick={toggleMenu} />
+        }
       </div>
-    </div>
+      <div>
+        {
+          isOpenMenu
+          ? (
+            <div className={styles.links_container_mobile}>
+              {/* <a className={styles.link} onClick={() => navigate('/about')}>Sobre mi</a> */}
+              <Link className={styles.link_mobile} href='/#about'>Sobre mi</Link>
+              <Link className={styles.link_mobile} href='/#projects'>Proyectos</Link>
+              <Link className={styles.link_mobile} href='/#contact'>Contácto</Link>
+            </div>
+          ) : null
+        }
+      </div>
+    </>
   )
 }
